005fae58db0d9eb8ede58f1d4937cc7b1b596363
Omar <muhammad.omer.1234.mo@gmail.com>
2025-12-12 16:18:20 +0500

    new name

--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,18 @@
 from typing import Optional
 from sqlmodel import SQLModel, Field
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String, Identity
 
 class ZTBL_User(SQLModel, table=True):
-    __tablename__ = "ztbl_user"
+    __tablename__ = "ztbl_user2"
 
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(
+        default=None,
+        sa_column=Column(
+            Integer,
+            Identity(start=1, increment=1),  # Oracle identity
+            primary_key=True,
+        ),
+    )
     email: str = Field(
         sa_column=Column(String(255), nullable=False, unique=True, index=True)
     )
